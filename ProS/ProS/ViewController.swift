c88f851b90d6cf58115bdcae251d3a88803fc22b
CH3821 <stzn14@gmail.com>
2017-02-06 16:53:46 +0800

    整理文件夹

--- ProS/ProS/ViewController.swift
+++ ProS/ProS/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // MARK: - Memory
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
